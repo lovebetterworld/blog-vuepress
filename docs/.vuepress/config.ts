e09f4d109eb1524db534c52ff2bdf060b1d15413
爱是与世界平行 <1090239782@qq.com>
2022-05-04 21:31:53 +0800

    学习笔记

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -260,8 +260,6 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
                 items: [
                     {text: '学习', link: '/pages/f2a556/'},
                     {text: '面试', link: '/pages/aea6571b7a8bae86/'},
-                    {text: '心情杂货', link: '/pages/2d615df9a36a98ed/'},
-                    {text: '实用技巧', link: '/pages/baaa02/'},
                     {text: '友情链接', link: '/friends/'},
                 ],
             },
