7fc4877b6cea1104e44572982b28ac868edc0ad4
AiShiYuShiJiePingXing <1090239782@qq.com>
2022-05-16 11:20:58 +0800

    学习笔记

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -288,13 +288,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
                 ]
             },
             {
-                text: '收藏',
-                link: '/pages/beb6c0bd8a66cea6/',
-            },
-            {
                 text: '索引',
                 link: '/archives/',
                 items: [
+                    {text: '收藏', link: '/pages/beb6c0bd8a66cea6/'},
                     {text: '分类', link: '/categories/'},
                     {text: '标签', link: '/tags/'},
                     {text: '归档', link: '/archives/'},
