cb4d55e9eb88279b07047190576e8b10fe7a5257
AiShiYuShiJiePingXing <1090239782@qq.com>
2022-05-16 11:30:28 +0800

    学习笔记

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -261,6 +261,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
                 text: '更多',
                 link: '/more/',
                 items: [
+                    {text: '工具', link: '/tool/typora/'},
                     {text: '学习', link: '/pages/f2a556/'},
                     {text: '面试', link: '/pages/aea6571b7a8bae86/'},
                     {text: '友情链接', link: '/friends/'},
