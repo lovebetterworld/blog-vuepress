867c01526ba5858880f3132ad144245cfa68860c
AiShiYuShiJiePingXing <1090239782@qq.com>
2022-09-27 17:33:41 +0800

    学习笔记

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -571,53 +571,12 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
             'vuepress-plugin-baidu-tongji', // 百度统计
             {hm: baiduCode,},
         ],
-        // 全文搜索。 ⚠️注意：此插件会在打开网站时多加载部分js文件用于搜索，导致初次访问网站变慢。如在意初次访问速度的话可以不使用此插件！（推荐：vuepress-plugin-thirdparty-search）
-        'fulltext-search',
-        [
-            'one-click-copy', // 代码块复制按钮
-            {
-                copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-                copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
-                duration: 1000, // prompt message display time.
-                showInMobile: false, // whether to display on the mobile side, default: false.
-            },
-        ],
-        [
-            'demo-block', // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
-            {
-                settings: {
-                    jsfiddle: false, // 是否显示 jsfiddle 链接
-                    codepen: true, // 是否显示 codepen 链接
-                    horizontal: false, // 是否展示为横向样式
-                },
-            },
-        ],
         [
             'vuepress-plugin-zooming', // 放大图片
             {
                 selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
                 options: {
                     bgColor: 'rgba(0,0,0,0.6)',
-                },
-            },
-        ],
-        [
-            'vuepress-plugin-comment', // 评论
-            {
-                choosen: 'gitalk',
-                options: {
-                    clientID: '096db831ba977f2d3d88',
-                    clientSecret: 'a13ea8f32051045e9f30206f5c9771fe7dfb04d2',
-                    repo: 'blog-vuepress', // GitHub 仓库
-                    owner: 'lovebetterworld', // GitHub仓库所有者
-                    admin: ['lovebetterworld'], // 对仓库有写权限的人
-                    // distractionFreeMode: true,
-                    pagerDirection: 'last', // 'first'正序 | 'last'倒序
-                    id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-                    title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-                    labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-                    body:
-                        '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
                 },
             },
         ],
